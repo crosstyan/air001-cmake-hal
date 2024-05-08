89a9db2781f5b22e41dc683312fdbb724c881fb4
crosstyan <crosstyan@outlook.com>
2024-05-08 11:39:42 +0800

    clang format and clangd

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,13 @@
 //
 // Created by Kurosu Chan on 2024/5/8.
 //
-#include<cstdio>
-#include<air001xx_hal.h>
+#include <air001xx_hal.h>
 
-int main()
-{
-    HAL_Init();
-    return 0;
+extern "C" {
+[[noreturn]] int main() {
+  HAL_Init();
+  for (;;) {
+    __NOP();
+  }
+}
 }
